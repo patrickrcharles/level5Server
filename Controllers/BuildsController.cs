b11ebb03b024015156ae9ab353ac156b18dad437
Patrick Charles <patrickrcharles@gmail.com>
2021-03-10 12:11:33 -0600

    builds controller+view

---
+++ Controllers/BuildsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mysql_scaffold_dbcontext_test.Controllers
+{
+    public class BuildsController : Controller
+    {
+        [Route("[controller]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

9329238400cc09a0caaf0526e18e0c2fcc882b1e
Patrick Charles <patrickrcharles@gmail.com>
2021-03-10 14:06:17 -0600

    Update ApplicationController.cs
    post, get versions api working

--- Controllers/Api/ApplicationController.cs
+++ Controllers/Api/ApplicationController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using mysql_scaffold_dbcontext_test.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,6 +22,17 @@ namespace mysql_scaffold_dbcontext_test.Controllers.Api
         // GET: /api/highscores
         // get all highscores
         [HttpGet("version")]
+        public async Task<ActionResult<IEnumerable<Application>>> GetAllVersions()
+        {
+            return await _context.Application.OrderByDescending(x => x.id)
+                .ToListAsync();
+                
+        }
+
+        //--------------------- HTTP GET ---------------------------------------------------
+        // GET: /api/highscores
+        // get all highscores
+        [HttpGet("version/current")]
         public ActionResult<Application> GetCurrentVersion()
         {
             var version = _context.Application
@@ -29,6 +40,29 @@ namespace mysql_scaffold_dbcontext_test.Controllers.Api
                 .LastOrDefault();
 
             return version;
+        }
+
+        //--------------------- HTTP POST new application version ---------------------------------------------------
+        // POST: api/Highscores
+        // "create new"
+        [Route("version")]
+        [HttpPost]
+        public async Task<ActionResult<Application>> PostHighscore(Application application)
+        {
+            //_context.Users.Where(e => e.Userid == highscores.Userid).Any();
+            // if empty username  or userid NOT in user table
+            if (string.IsNullOrEmpty(application.CurrentVersion) 
+                || _context.Application.Where(e => e.CurrentVersion == application.CurrentVersion).Any())
+            {
+                return BadRequest();
+            }
+            else
+            {
+                _context.Application.Add(application);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction(nameof(GetAllVersions), new { id = application.id }, application);
+            }
         }
     }
 }

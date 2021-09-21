c467a2adc08e5ac17d202e53ad6af8ea0b61f322
Patrick Charles <patrickrcharles@gmail.com>
2021-09-21 10:33:34 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using mysql_scaffold_dbcontext_test.Models.level5;
 using System;
 using System.Collections.Generic;
 using System.Linq;

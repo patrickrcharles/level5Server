6f4883ff85ad55d57d69815e61d93661652e7a54
Patrick Charles <patrickrcharles@gmail.com>
2021-09-21 10:33:23 -0500

    add user reports controller and view

---
+++ Controllers/level5/UserReportsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+
+namespace mysql_scaffold_dbcontext_test.Models.level5
+{
+    [Route("[controller]")]
+    public class UserReportsController : Controller
+    {
+        private readonly Level5Context _context;
+
+        public UserReportsController(Level5Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<IActionResult> UserReports()
+        {
+            return View(await _context.UserReports.OrderByDescending(x => x.Date).ToListAsync());
+        }
+    }
+}
